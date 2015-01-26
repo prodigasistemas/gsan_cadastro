7650fb46abb5ac11dd7214461341b45aaba17d38
Paulo Moura <paulociecomp@gmail.com>
2015-01-25 21:55:30 -0300

    teste do controller pessoa sexos

---
+++ spec/controllers/pessoa_sexos_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe PessoaSexosController, type: :controller do
+  render_views
+
+  describe "GET index" do
+    let!(:sexos) { create_list(:pessoa_sexo, 2) }
+
+    it "retorna lista de tipos de sexos" do
+      get :index, format: :json
+      expect(json.size).to eq sexos.size
+    end
+  end
+end
---
+++ spec/support/factories/pessoa_sexo_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :pessoa_sexo do
+    descricao "Indefinido"
+    ativo 1
+  end
+end
