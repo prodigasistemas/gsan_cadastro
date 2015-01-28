a34fe607a0b4493ac35838fdc913aa63d87d735c
Felipe Iketani <felipeiketani@gmail.com>
2015-01-28 12:05:44 -0300

    Testando esferas do poder completamente

---
+++ spec/controllers/esferas_poder_controller_spec.rb
@@ -0,0 +1,121 @@
+require 'rails_helper'
+
+describe EsferasPoderController, type: :controller do
+  render_views
+
+  let(:json) { JSON.parse(response.body) }
+  let!(:esferas_poder) { create_list(:esfera_poder, 3) }
+
+  describe "GET index" do
+    before do
+      get :index, params, format: :json
+
+      expect(response).to be_success
+      expect(json['page']['first_page']).to be true
+      expect(json['page']['last_page']).to be true
+      expect(json['page']['current_page']).to eq(1)
+    end
+
+    context "quando a consulta retorna resultados" do
+      let(:params) do
+        {
+          "query"=>
+            {
+              "descricao"=>"Esfera"
+            }
+        }
+      end
+
+      it "retorna dados da paginacao" do
+        expect(json['page']['total']).to eq(3)
+        expect(json['page']['total_pages']).to eq(1)
+      end
+
+      it "retorna lista de ceps" do
+        expect(json['esferas_poder'].size).to eq(3)
+        expect(json['esferas_poder'].collect{|l| l["descricao"]}).to include(esferas_poder.first.descricao)
+      end
+    end
+
+    context "quando a consulta não retorna resultados" do
+      let(:params) do
+        {
+          "query"=>
+            {
+              "descricao"=>"Nenhuma esfera aqui"
+            }
+        }
+      end
+
+      it "retorna dados da paginacao" do
+        expect(json['page']['total']).to eq(0)
+        expect(json['page']['total_pages']).to eq(0)
+      end
+
+      it "retorna lista de ceps vazia" do
+        expect(json['esferas_poder'].size).to eq(0)
+      end
+    end
+  end
+
+  describe "GET show" do
+    it "retorna um cep" do
+      get :show, id: 1, format: :json
+      expect(json['descricao']).to eq esferas_poder.first.descricao
+    end
+  end
+
+  describe "POST create" do
+    context "quando cep é criado com sucesso" do
+      let(:params) {
+        {
+          'esfera_poder'=>attributes_for(:esfera_poder).with_indifferent_access
+        }
+      }
+
+      it "retorna a esfera poder" do
+        post :create, params, format: :json
+        expect(response).to be_success
+      end
+    end
+
+    context "quando cep não é criado" do
+      let(:params) {
+        {
+          'esfera_poder'=>attributes_for(:esfera_poder, descricao: '').with_indifferent_access
+        }
+      }
+
+      it "retorna erros" do
+        post :create, params, format: :json
+        expect(response.status).to eq 422
+        expect(json['errors']).to_not be_nil
+      end
+    end
+  end
+
+  describe "PUT update" do
+    context "quando esfera de poder é atualizado com sucesso" do
+      let(:params) {
+        attributes_for(:esfera_poder).with_indifferent_access
+      }
+
+      it "retorna o cep" do
+        put :update, id: 1, esfera_poder: params, format: :json
+        expect(response).to be_success
+      end
+    end
+
+    context "quando esfera de poder não é atualizado" do
+      let(:params) {
+        attributes_for(:esfera_poder, descricao: '').with_indifferent_access
+      }
+
+      it "retorna erros" do
+        put :update, id: 1, esfera_poder: params, format: :json
+        expect(response.status).to eq 422
+        expect(json['errors']).to_not be_nil
+      end
+    end
+  end
+end
--- spec/models/esfera_poder_spec.rb
+++ spec/models/esfera_poder_spec.rb
@@ -20,4 +20,34 @@ describe EsferaPoder do
     it { should_not allow_value(0).for :permite_gerar_certidao_negativa_cliente }
     it { should_not allow_value(3).for :permite_gerar_certidao_negativa_cliente }
   end
+
+  describe "escopos" do
+    let!(:esfera_poder_1) { create(:esfera_poder) }
+    let!(:esfera_poder_2) { create(:esfera_poder) }
+    let!(:esfera_poder_3) { create(:esfera_poder) }
+
+    describe ".descricao" do
+      let!(:esfera_poder_4) { create(:esfera_poder, descricao: "Nao tenho descricao") }
+      it "retorna esferas do poder pela descricao" do
+        expect(EsferaPoder.descricao("Esfera")).to     include(esfera_poder_1, esfera_poder_2, esfera_poder_3)
+        expect(EsferaPoder.descricao("Esfera")).to_not include(esfera_poder_4)
+      end
+    end
+
+    describe ".permite_gerar_certidao_negativa_imovel" do
+      let!(:esfera_poder_4) { create(:esfera_poder, permite_gerar_certidao_negativa_imovel: 1) }
+      it "retorna esferas do poder pela permite_gerar_certidao_negativa_imovel" do
+        expect(EsferaPoder.permite_gerar_certidao_negativa_imovel(2)).to     include(esfera_poder_1, esfera_poder_2, esfera_poder_3)
+        expect(EsferaPoder.permite_gerar_certidao_negativa_imovel(2)).to_not include(esfera_poder_4)
+      end
+    end
+
+    describe ".permite_gerar_certidao_negativa_cliente" do
+      let!(:esfera_poder_4) { create(:esfera_poder, permite_gerar_certidao_negativa_cliente: 1) }
+      it "retorna esferas do poder pela permite_gerar_certidao_negativa_cliente" do
+        expect(EsferaPoder.permite_gerar_certidao_negativa_cliente(2)).to     include(esfera_poder_1, esfera_poder_2, esfera_poder_3)
+        expect(EsferaPoder.permite_gerar_certidao_negativa_cliente(2)).to_not include(esfera_poder_4)
+      end
+    end
+  end
 end
