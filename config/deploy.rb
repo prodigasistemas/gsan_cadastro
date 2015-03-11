d60f41907c23f3251461f5f6e34deb55ecfb2cd5
Luiz Sanches <luizgrsanches@gmail.com>
2015-03-11 17:48:44 -0300

    correcao do nome da aplicacao na configuracao do capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'gsan_cadsatro'
+set :application, 'gsan_cadastro'
 set :repo_url, 'git@github.com:prodigasistemas/gsan_cadastro.git'
 
 # Default branch is :master
