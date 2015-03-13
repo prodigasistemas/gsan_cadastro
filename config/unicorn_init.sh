5846b879904584f716bb3a3363003f4c626fc41a
Luiz Sanches <luizgrsanches@gmail.com>
2015-03-13 18:35:08 -0300

    deploy com o usuario jenkins

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/gsan_cadastro/current
 PID=/tmp/.unicorn.gsan_cadastro.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deploy
+AS_USER=jenkins
 set -u
 
 OLD_PIN="$PID.oldbin"
