42b0f7015fb60399d66391fed06b20837b90b09e
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2016-07-12 15:13:17 -0300

    Implementacao da autenticacao do um usuario
    De acordo com os parametros fornecidos, o retorno da autenticacao podera ser:
    . 200 - ok
    . 401 - unauthorized
    
    O teste aborda esse tratamento

---
+++ spec/controllers/autenticacao_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe AutenticacaoController, type: :controller do
+  #senha:123456
+  let!(:usuario)                  { create(:usuario, login: 'usuario', senha: 'fEqNCco3Yq9h5ZUglD3CZJT4lBs=') } 
+
+  describe "POST create" do
+    context "dados validos de autenticacao" do
+      let(:params) do
+        {
+          "login"  => "usuario",
+          "senha" => "123456"
+        }
+      end
+
+      before do
+        post :create, params
+      end
+
+      it "autenticacao realizada com sucesso" do
+        expect(response).to be_success
+        body = JSON.parse(response.body)
+        expect(body['message']).to eql('Autenticacao realizada com sucesso!')
+      end
+    end
+
+    context "dados incorretos de autenticacao" do
+      let(:params) do
+        {
+          "login"  => "usuario",
+          "senha" => "senha_nao_confere"
+        }
+      end
+
+      before do
+        post :create, params
+      end
+
+      it "autenticacao realizada com sucesso" do
+        expect(response).to be_unauthorized
+        body = JSON.parse(response.body)
+        expect(body['message']).to eql('Falha ao autenticar usuario!')
+      end
+    end    
+  end  
+end
---
+++ spec/support/factories/usuario_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :usuario do
+    id
+  end
+end
