e2f99a5be017714f5b6135fb9b5dfc0034ce5ca7
Mateus Linhares <mateuslinhares@gmail.com>
2016-07-19 16:26:04 -0300

    adicionei endpoint para filtro de um recurso

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -2,12 +2,32 @@ module Filterable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def filter(filtering_params)
+    def filter(filtering_params = {})
+      query = normalize_query(filtering_params)
+
       results = self.where(nil)
-      filtering_params.each do |key, value|
-        results = results.public_send(key, value) if value.present?
-      end
+      results = results.where(query) if query.present?
+
       results
+    end
+
+    def filterable_fields
+      self.attribute_aliases.keys.map(&:to_sym)
+    end
+
+    private
+
+    def normalize_query(params)
+      params = params.deep_symbolize_keys
+      query = []
+
+      params.each_pair do |key, value|
+        next unless attribute = attribute_alias(key)
+
+        query << "cast(#{self.table_name}.#{attribute} as text) ILIKE '%#{value}%'" if value.present?
+      end
+
+      query.compact.join(" OR ")
     end
   end
 end
--- spec/concerns/filterable_spec.rb
+++ spec/concerns/filterable_spec.rb
@@ -4,7 +4,7 @@ describe Filterable do
   let!(:cep_tipo) { create(:cep_tipo) }
   let!(:cep)      { create(:cep, logradouro: "MORUMBI") }
   let!(:cep_2)    { create(:cep, logradouro: 'qualquer outro nome') }
-  let(:params)    { { filtro_logradouro: 'MORUMBI' } }
+  let(:params)    { { logradouro: 'MORUMBI' } }
 
   it "retorna os registros do filtro" do
     ceps = Cep.filter(params)
