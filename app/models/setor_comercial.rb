1579b23c5504e88e391980c62494763f4a1b690e
Paulo Moura <paulociecomp@gmail.com>
2016-07-28 15:43:51 -0300

    adicao de atributo localidade_id no model SetorComercial

--- app/models/setor_comercial.rb
+++ app/models/setor_comercial.rb
@@ -6,8 +6,9 @@ class SetorComercial < ActiveRecord::Base
   self.table_name  = 'cadastro.setor_comercial'
   self.primary_key = 'stcm_id'
 
-  alias_attribute "id", "stcm_id"
-  alias_attribute "nome", "stcm_nmsetorcomercial"
+  alias_attribute "id",            "stcm_id"
+  alias_attribute "nome",          "stcm_nmsetorcomercial"
+  alias_attribute "localidade_id", "loca_id"
 
   has_many :imoveis, foreign_key: :stcm_id
   has_many :rotas,   foreign_key: :stcm_id
