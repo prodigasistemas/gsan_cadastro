f45a4231eddb8e4b7c0b38b58203d4f9da2d0343
Paulo Moura <paulociecomp@gmail.com>
2016-08-02 16:35:41 -0300

    listando rotas com order by

--- app/controllers/rotas_controller.rb
+++ app/controllers/rotas_controller.rb
@@ -1,6 +1,6 @@
 class RotasController < ApplicationController
   def index
-    @rotas = Rota.setor_comercial_id(params[:filtros][:setor_comercial_id])
+    @rotas = Rota.setor_comercial_id(params[:filtros][:setor_comercial_id]).order(:codigo)
 
     render json: { entidades: @rotas.map(&:atributos) }
   end
