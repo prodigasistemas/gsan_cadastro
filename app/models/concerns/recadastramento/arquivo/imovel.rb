63adf7438a24cd1250a261b82fa5640c8a981655
Paulo Igor <piagodinho@gmail.com>
2016-08-04 15:43:45 -0300

    mapeamento imovel

---
+++ app/models/concerns/recadastramento/arquivo/imovel.rb
@@ -0,0 +1,17 @@
+module Recadastramento
+  module Arquivo
+    class Imovel
+      attr_accessor :matricula, :tipo_operacao, :codigo_cliente, :inscricao,
+                    :rota, :face, :codigo_municipio, :numero_iptu, :numero_celpa,
+                    :numero_pontos_uteis, :numero_ocupantes, :tipo_logradouro_imovel,
+                    :logradouro_imovel, :numero_imovel, :complemento_imovel,
+                    :bairro_imovel, :cep_imovel, :municipio_imovel, :codigo_logradouro_imovel,
+                    :sub_categoria_residenciais, :sub_categoria_comerciais, :sub_categoria_publicas,
+                    :sub_categoria_industriais, :classe_social, :area_construida, :tipo_ligacao,
+                    :quantidade_animais_domesticos, :volume_cisterna, :volume_piscina, :volume_caixa_dagua,
+                    :tipo_dormitorio, :fonte_alternativa, :economias_social, :economias_outros,
+                    :quantidade_criancas, :quantidade_adultos, :quantidade_idosos, :quantidade_empregados,
+                    :quantidade_alunos, :quantidade_caes, :quantidade_outros
+    end
+  end
+end
