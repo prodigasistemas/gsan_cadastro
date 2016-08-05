b87862223f74cad00a9cbaec28f60eee47c9b343
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2016-08-05 17:52:53 -0300

    Relacionando funcionalidades com grupos e operacoes

--- app/models/funcionalidade.rb
+++ app/models/funcionalidade.rb
@@ -2,9 +2,19 @@ class Funcionalidade < ActiveRecord::Base
   self.table_name  = "seguranca.funcionalidade"
   self.primary_key = "fncd_id"
 
-  alias_attribute "descricao",     "fncd_dsfuncionalidade"
+  alias_attribute "id",             "fncd_id"
+  alias_attribute "descricao",      "fncd_dsfuncionalidade"
   alias_attribute "modulo_id",      "modu_id"
   alias_attribute "atualizado_em",  "fncd_tmultimaalteracao"  
 
-  belongs_to :modulo, foreign_key: :modu_id
+  belongs_to :modulo,                   foreign_key: :modu_id
+  belongs_to :funcionalidade_categoria, foreign_key: :fncg_id
+
+  has_many :grupo_funcionalidade_operacoes, foreign_key: :fncd_id
+
+  scope :funcionalidades_grupo, -> (grupo_id) {
+    joins(:grupo_funcionalidade_operacoes)
+    .where(grupo_func_operacao: {grup_id: grupo_id})
+    .group("seguranca.funcionalidade.fncd_id")
+  }  
 end
