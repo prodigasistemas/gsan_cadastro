82172b1efc750f815a620ca5cc35433dc2e60c1e
Paulo Moura <paulociecomp@gmail.com>
2016-08-08 14:49:59 -0300

    ordenacao de lista de setores comerciais

--- app/controllers/setor_comercial_controller.rb
+++ app/controllers/setor_comercial_controller.rb
@@ -1,6 +1,6 @@
 class SetorComercialController < ApplicationController
   def index
-    @setores = SetorComercial.localidade_id(params[:filtros][:localidade_id])
+    @setores = SetorComercial.localidade_id(params[:filtros][:localidade_id]).order(:nome)
 
     render json: { entidades: @setores.map(&:atributos) }
   end
