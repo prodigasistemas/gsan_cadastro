6693707734a756c19c09fbf3043318b8ddf6c92c
Paulo Igor <piagodinho@gmail.com>
2016-08-11 14:36:28 -0300

    teste de cenários para possui medidor

--- spec/models/concerns/recadastramento/arquivo/hidrometro_spec.rb
+++ spec/models/concerns/recadastramento/arquivo/hidrometro_spec.rb
@@ -15,4 +15,19 @@ describe Recadastramento::Arquivo::Hidrometro do
     expect(hidrometro_arquivo.descricao_protecao).to eq(hidrometro_protecao.descricao_protecao)
     expect(hidrometro_arquivo.possui_medidor).to be true
   end
+
+  it '#hidrometro_infos sem medidor' do
+    hidrometro_arquivo = Recadastramento::Arquivo::Hidrometro.new hidrometro
+
+    expect(hidrometro_arquivo.possui_medidor).to be false
+  end
+
+  it '#hidrometro_infos com medidor e sem numero' do
+    hidrometro.numero_hidrometro = nil
+    hidrometro.save
+
+    hidrometro_arquivo = Recadastramento::Arquivo::Hidrometro.new hidrometro, hidrometro_marca, hidrometro_capacidade, hidrometro_protecao
+
+    expect(hidrometro_arquivo.possui_medidor).to be true
+  end
 end
