75a4d0e4c4345d474eb28cace5d47472d1ada1d5
Paulo Igor <piagodinho@gmail.com>
2016-09-14 15:44:53 -0300

    adicionado campo com codigo da rota

--- app/models/concerns/recadastramento/arquivo/imovel.rb
+++ app/models/concerns/recadastramento/arquivo/imovel.rb
@@ -12,7 +12,7 @@ module Recadastramento
                     :quantidade_outros, :rota, :ramal_local_instalacao_nome, :situacao_atualizacao_cadastral_nome,
                     :area_construida, :volume_piscina, :codigo_setor_comercial, :cadastro_ocorrencia_nome,
                     :comentarios, :datahora_geracao, :datahora_retorno, :datahora_aprovacao,
-                    :datahora_processamento, :percentual_abastecimento
+                    :datahora_processamento, :percentual_abastecimento, :codigo_rota_id
     end
   end
 end
