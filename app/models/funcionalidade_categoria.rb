915b22b05f1bf934d301a9b8614df6f0d0e32712
Paulo Igor <piagodinho@gmail.com>
2016-09-21 17:50:05 -0300

    correção de bug na montagem do menu

--- app/models/funcionalidade_categoria.rb
+++ app/models/funcionalidade_categoria.rb
@@ -1,7 +1,7 @@
 class FuncionalidadeCategoria < ActiveRecord::Base
   include IncrementableId
   include Filterable
-  
+
   self.table_name  = "seguranca.funcionalidade_categoria"
   self.primary_key = "fncg_id"
 
@@ -9,10 +9,31 @@ class FuncionalidadeCategoria < ActiveRecord::Base
   alias_attribute "descricao",      "fncg_dsfuncionalidadecategoria"
   alias_attribute "superior",       "fncg_idsuperior"
   alias_attribute "em_uso",         "fncg_icuso"
-  alias_attribute "modulo_id",      "modu_id"  
-  alias_attribute "atualizado_em",  "fncg_tmultimaalteracao"  
+  alias_attribute "modulo_id",      "modu_id"
+  alias_attribute "atualizado_em",  "fncg_tmultimaalteracao"
 
   belongs_to :modulo, foreign_key: :modu_id
+  belongs_to :topitem, class_name: "FuncionalidadeCategoria", foreign_key: "fncg_idsuperior"
+  has_many :funcionalidades, foreign_key: :fncg_id
 
   has_many :subitens, class_name: "FuncionalidadeCategoria", foreign_key: "fncg_idsuperior"
+
+  scope :para_as_funcionalidades, -> (funcionalidades_id) {
+    joins(:funcionalidades)
+    .where('funcionalidade.fncd_id in (?)', funcionalidades_id)
+    .group('funcionalidade_categoria.fncg_id')
+  }
+
+  def categorias_superiores
+    get_categorias_superiores(topitem, [])
+  end
+
+  private
+
+  def get_categorias_superiores(item, itens)
+    return itens if item.nil?
+    itens << item
+    get_categorias_superiores(item.topitem, itens)
+    itens
+  end
 end
--- app/presenters/menu_presenter.rb
+++ app/presenters/menu_presenter.rb
@@ -9,14 +9,15 @@ class MenuPresenter
 
   def constroi_arvore
     categoria_root
-
     preenche_menu(menu, @categoria, @funcionalidades.to_a)
   end
 
   def preenche_menu (item_menu, categoria, funcionalidades)
+    funcionalidades_incluidas = []
+
     funcionalidades.each do |funcionalidade|
-      if funcionalidade.funcionalidade_categoria_id == categoria.id
-        funcionalidades.delete funcionalidade
+      if categoria.funcionalidades.include? funcionalidade
+        funcionalidades_incluidas << funcionalidade
         acao = Menu.new(funcionalidade.descricao, funcionalidade.caminho)
         item_menu.submenu << acao
         item_menu.url = ""
@@ -24,11 +25,15 @@ class MenuPresenter
       end
     end
 
+    funcionalidades.delete funcionalidades_incluidas
+
     categoria.subitens.each do |item|
-      submenu = Menu.new(item.descricao)
-      item_menu.submenu << submenu
-      item_menu.id = @count += 1
-      preenche_menu(submenu, item, funcionalidades)
+      if categoria_acessivel? item
+        submenu = Menu.new(item.descricao)
+        item_menu.submenu << submenu
+        item_menu.id = @count += 1
+        preenche_menu(submenu, item, funcionalidades)
+      end
     end
 
     menu
@@ -36,11 +41,22 @@ class MenuPresenter
 
   private
 
+  def categoria_acessivel? categoria
+    @categorias_acessiveis.each do |categoria_acessivel|
+      ids_acessiveis = categoria_acessivel.categorias_superiores.map(&:id)
+      ids_acessiveis << categoria_acessivel.id
+
+      return true if ids_acessiveis.include? categoria.id
+    end
+
+    false
+  end
+
   def categoria_root
     grupos = UsuarioGrupo.where(usuario_id: @usuario_id)
 
     @funcionalidades = Funcionalidade.funcionalidades_grupo(grupos.map(&:grup_id))
-
+    @categorias_acessiveis = FuncionalidadeCategoria.para_as_funcionalidades(@funcionalidades.map(&:id))
     @categoria = FuncionalidadeCategoria.includes(subitens: [subitens: [subitens: [:subitens]]]).find_by(superior: nil)
   end
 
