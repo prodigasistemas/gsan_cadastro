2c05306c7e7c8c95e7d35ebf638756c58f754642
Felipe <felipesantos2089@gmail.com>
2018-06-26 14:39:34 -0300

    mapeando campos da tabela cadastro.imovel_atlz_cadastral

---
+++ app/models/imovel_atualizacao_cadastral.rb
@@ -0,0 +1,13 @@
+class ImovelAtualizacaoCadastral < ActiveRecord::Base
+  include IncrementableId
+  include API::Model
+
+  self.table_name  = 'cadastro.imovel_atlz_cadastral'
+  self.primary_key = 'imov_id'
+
+  alias_attribute :id,                            :imov_id
+  alias_attribute :hidrometro_numero,             :imac_nnhidrometro
+  alias_attribute :ligacao_agua_situacao_id,      :last_id
+  alias_attribute :ligacao_esgoto_situacao_id,    :lest_id
+
+end
