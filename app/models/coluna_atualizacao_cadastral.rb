4d72a6fa8a7f511f0e3c12cad6077148aab709e7
Ewertton Bravo <ewerttonbravo@gmail.com>
2018-08-13 09:50:29 -0300

    Alterando nomes das colunas para incluir valuna com valor revisado

--- app/models/coluna_atualizacao_cadastral.rb
+++ app/models/coluna_atualizacao_cadastral.rb
@@ -10,11 +10,12 @@ class ColunaAtualizacaoCadastral < ActiveRecord::Base
   alias_attribute :atualizacao_cadastral_id, :tatc_id
   alias_attribute :tabela_coluna_id,         :tbco_id
   alias_attribute :valor_anterior,           :tcac_cnvaloranterior
-  alias_attribute :valor_atual,              :tcac_cnvaloratual
+  alias_attribute :valor_atual,              :tcac_cnvalortransmitido
   alias_attribute :autorizado,               :tcac_icautorizado
   alias_attribute :data_processamento,       :tcac_dtvalidacao
   alias_attribute :data_ultima_atualizacao,  :tcac_ultimaatualizacao
   alias_attribute :usuario_id,               :usur_id
+  alias_attribute :valor_revisado,           :tcac_cnvalorrevisado
 
   belongs_to :atualizacao_cadastral, foreign_key: :tatc_id
   belongs_to :tabela_coluna,         -> { includes(:tabela) }, foreign_key: :tbco_id
