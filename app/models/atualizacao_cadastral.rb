6628ed2a559085e87decb495b524708bf48d27df
Pamela Gatinho <pamelagatinho@gmail.com>
2018-12-05 14:20:38 -0300

    Correcao no filtro de setor/rota

--- app/models/atualizacao_cadastral.rb
+++ app/models/atualizacao_cadastral.rb
@@ -61,10 +61,10 @@ class AtualizacaoCadastral < ActiveRecord::Base
       query << "\nand txac.loca_id between #{params[:localidade_id_inicial]} and #{params[:localidade_id_final]}"
     end
     unless params[:setor_comercial_id_inicial].blank? or params[:setor_comercial_id_final].blank?
-      query << "\nand txac.txac_cdsetorcomercial between #{params[:setor_comercial_id_inicial]} and #{params[:setor_comercial_id_final]}"
+      query << "\nand rota.stcm_id between #{params[:setor_comercial_id_inicial]} and #{params[:setor_comercial_id_final]}"
     end
     unless params[:rota_id_inicial].blank? or params[:rota_id_final].blank?
-      query << "\nand rota.rota_cdrota between #{params[:rota_id_inicial]} and #{params[:rota_id_final]}"
+      query << "\nand rota.rota_id between #{params[:rota_id_inicial]} and #{params[:rota_id_final]}"
     end
 
     situacoes = ""
