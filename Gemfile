df67e1ed98bc0c6c6ecc2aa692888c655210a805
Mateus Linhares <mateuslinhares@gmail.com>
2019-07-25 15:12:08 -0300

    Atualização para Rails 5.2.3

--- Gemfile
+++ Gemfile
@@ -1,14 +1,15 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.2.7'
+gem 'rails', '5.2.3'
 gem 'pg'
 gem 'jbuilder', '~> 2.0'
 gem 'rack-cors', :require => 'rack/cors'
 gem 'kaminari'
 gem 'dotenv-rails'
-gem 'composite_primary_keys', '~> 8.0'
+gem 'composite_primary_keys', '11.2.0'
 gem 'bulk_insert'
 gem 'sucker_punch'
+gem 'bootsnap'
 
 gem 'log_file', github: 'prodigasistemas/log_file'
 gem 'exception_notification'
@@ -30,8 +31,11 @@ group :production do
 end
 
 group :development do
-  gem 'byebug'
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'byebug'
   gem 'thin'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,15 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
+# require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -18,21 +21,21 @@ Dotenv::Railtie.load
 
 module GsanApi
   class Application < Rails::Application
-    config.active_record.raise_in_transactional_callbacks = true
+    config.load_defaults 5.1
+
     config.i18n.load_path += Dir["config/locales/**/*.yml"]
     config.i18n.default_locale = "pt-BR"
     config.time_zone = 'Buenos Aires'
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_attributes = false
     config.i18n.available_locales = ["pt-BR"]
-    config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths << Rails.root.join('app/services')
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
 
