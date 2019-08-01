85d996cef7a5eee5c67e871be3cbfbededbd87af
Mateus Linhares <mateuslinhares@gmail.com>
2019-08-01 12:13:59 -0300

    Adicionei o .bundle nos direitorios do capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets tmp/sessions public/assets}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets tmp/sessions public/assets} << '.bundle'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
