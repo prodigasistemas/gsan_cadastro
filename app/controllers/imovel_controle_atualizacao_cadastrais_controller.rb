c4331c50f8e8f5de9f6e500d5990d5d9a36f0443
Ewertton Bravo <ewerttonbravo@gmail.com>
2019-09-09 16:51:33 -0300

    Adicionando consulta com SQL Nativo para imoveis que foram reprovados

--- app/controllers/imovel_controle_atualizacao_cadastrais_controller.rb
+++ app/controllers/imovel_controle_atualizacao_cadastrais_controller.rb
@@ -1,6 +1,25 @@
 class ImovelControleAtualizacaoCadastraisController < ApplicationController
 
-  before_action :find_imovel_controle_atualizacao_cadastral
+  before_action :find_imovel_controle_atualizacao_cadastral, only: [:show, :update]
+
+  def index
+    @reprovados = []
+    params[:query].delete :per_page if params[:query].present?
+    params[:query].delete :page if params[:query].present?
+
+    if params[:query].present? and params[:query][:reprovados].present?
+      @reprovados = ImovelControleAtualizacaoCadastral.reprovados_filtro(params[:query])
+      if @reprovados.try(:any?)
+        @total = @reprovados.count
+        render json: { entidades: @reprovados.map {|rep| Recadastramento::ReprovadoDto.new(rep) },
+                       total: @total }, status: :ok
+      else
+        render json: { entidades: [], total: 0 }, status: :not_found
+      end
+    else
+      render json: { entidades: [], total: 0 }, status: :not_found
+    end
+  end
 
   def show
     @imovel_controle_atualizacao_cadastral = ImovelControleAtualizacaoCadastral.find_by(imovel_id: params[:id]) if @imovel_controle_atualizacao_cadastral.nil?
--- app/models/imovel_controle_atualizacao_cadastral.rb
+++ app/models/imovel_controle_atualizacao_cadastral.rb
@@ -16,12 +16,15 @@ class ImovelControleAtualizacaoCadastral < ActiveRecord::Base
   alias_attribute "tempo_processamento", "icac_tmprocessamento"
   alias_attribute "cadastro_imovel_id", "cocr_id"
   alias_attribute "data_hora_pre_aprovacao", "icac_tmpreaprovacao"
+  alias_attribute "data_hora_reprovacao", "icac_tmreprovacaolote"
+  alias_attribute "lote", "icac_lote"
 
   belongs_to :cadastro_ocorrencia, foreign_key: "cocr_id", optional: true
   belongs_to :imovel, foreign_key: "imov_id"
   belongs_to :imovel_retorno, foreign_key: "imre_id"
   belongs_to :situacao_atualizacao_cadastral, foreign_key: "siac_id"
   has_many :visitas, foreign_key: :icac_id
+  has_one :atualizacao_cadastral, foreign_key: :imov_id
 
   scope :podem_ser_pre_aprovados, -> { where(situacao_atualizacao_cadastral_id: [ SituacaoAtualizacaoCadastral::SITUACOES[:"TRANSMITIDO"],
                                                                                   SituacaoAtualizacaoCadastral::SITUACOES[:"REVISADO"],
@@ -29,6 +32,8 @@ class ImovelControleAtualizacaoCadastral < ActiveRecord::Base
   scope :podem_ficar_em_revisao, -> { where(situacao_atualizacao_cadastral_id: SituacaoAtualizacaoCadastral::SITUACOES[:"TRANSMITIDO"]) }
   scope :podem_ser_pre_aprovados_em_lote, -> { where(situacao_atualizacao_cadastral_id: [ SituacaoAtualizacaoCadastral::SITUACOES[:"TRANSMITIDO"],
                                                                                           SituacaoAtualizacaoCadastral::SITUACOES[:"REVISITA"] ] ) }
+
+  scope :reprovados, -> { where("icac_tmreprovacaolote is not null") }
 
   def is_situacao_do_gsan?
     SituacaoAtualizacaoCadastral::GSAN_SITUACOES.include? situacao_atualizacao_cadastral_id
@@ -74,5 +79,37 @@ class ImovelControleAtualizacaoCadastral < ActiveRecord::Base
       end
       true
     end
+  end
+
+  def self.reprovados_filtro(params)
+    empresa_id = params[:empresa_id] || 0
+    query = <<-SQL
+      select ic.icac_lote as lote,
+             ic.imov_id as imovel_id,
+             cocr.cocr_dscadastroocorrencia as ocorrencia_cadastro,
+             u.usur_nmusuario as agente_cadastral,
+             ic.icac_tmreprovacaolote as data_hora_reprovacao
+      from atualizacaocadastral.imovel_controle_atlz_cad ic
+      inner join atualizacaocadastral.visita vis on vis.icac_id = ic.icac_id
+      inner join cadastro.cadastro_ocorrencia cocr on cocr.cocr_id = vis.cocr_id
+      inner join cadastro.imovel_atlz_cadastral iatlz on iatlz.imov_id = ic.imov_id
+      left join seguranca.usuario u on u.usur_id = vis.usur_id
+      where 1 = 1
+          and icac_tmreprovacaolote is not null
+          and vis.vist_id = (select max(v2.vist_id) from atualizacaocadastral.visita v2 where v2.icac_id = ic.icac_id)
+          and iatlz.empr_id = #{empresa_id}
+    SQL
+    query << "\nand vis.usur_id = #{params[:leiturista_id]}" unless params[:leiturista_id].blank?
+    query << "\nand ic.icac_lote = #{params[:lote]}" unless params[:lote].blank?
+    query << "\nand ic.imov_id = #{params[:matricula]}" unless params[:matricula].blank?
+    query << "\nand ic.icac_tmreprovacaolote::Date = '#{params[:data_hora_reprovacao].try(:to_date).try(:strftime)}'" unless params[:data_hora_reprovacao].blank?
+
+    if params[:ocorrencias_cadastro].present? and
+        params[:ocorrencias_cadastro] != AtualizacaoCadastral::TODOS
+      query << "\nand cocr.cocr_icvalidacao = #{params[:ocorrencias_cadastro]}"
+      query << "\nand vis.cocr_id = #{params[:cadastro_ocorrencia_id]}" if params[:cadastro_ocorrencia_id].present?
+    end
+
+    ActiveRecord::Base.connection.execute("#{query}\norder by ic.icac_tmreprovacaolote, ic.icac_lote, ic.imov_id, u.usur_nmusuario")
   end
 end
