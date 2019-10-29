b2faa639d6c321f98e5f6644f18da7988316f892
Ricardo Casseb <rcasseb@rcasseb.local>
2019-10-29 18:53:30 -0300

    devolucoes de guias

--- app/models/atendimento/devolucoes_imovel.rb
+++ app/models/atendimento/devolucoes_imovel.rb
@@ -1,13 +1,82 @@
 class Atendimento::DevolucoesImovel < Imovel
   def devolucoes
     cadastro = {}    
+    
+    contas  = []
+    contas_historico  = []
+    guias   = []
+    guias_historico   = []
+    debitos = []
+    debitos_historico = []
+
+    preenche_dados(Devolucao.por_imovel(self.id),contas,guias,debitos)
+    preenche_dados(DevolucaoHistorico.por_imovel(self.id),contas_historico,guias_historico,debitos_historico)
+
+    cadastro[:contas]  = contas
+    cadastro[:contas_historico]  = contas_historico
+    cadastro[:guias]   = guias
+    cadastro[:guias_historico]   = guias_historico
+    cadastro[:debitos] = debitos
+    cadastro[:debitos_historico] = debitos_historico
 
     cadastro
   end
 
   private 
 
-  def preenche_conta
+  def preenche_dados(devolucoes, contas = [], guias = [], debitos = [])    
+    devolucoes.each do |dev|  
+      if(dev.guia_devolucao.present?)
+        if(dev.guia_devolucao.documento_tipo.id == DocumentoTipo::TIPO[:conta])
+          contas << preenche_conta(dev)
+        elsif(dev.guia_devolucao.documento_tipo.id == DocumentoTipo::TIPO[:guia_pagamento])
+          guias << preenche_guia(dev)
+        end
+      else
+        if(dev.ano_mes_referencia_devolucao.present?)
+          contas << preenche_conta(dev)
+        else
 
+        end
+      end
+    end 
+  end
+
+  def preenche_conta(devolucao)
+    conta = {}
+
+    conta[:mes_ano] = devolucao.ano_mes_referencia_devolucao
+    conta[:valor_conta] = nil
+    
+    complemento(conta, devolucao)
+  end
+
+  def preenche_guia(devolucao)
+    guia = {}
+
+    cliente = nil
+    guia_devolucao = devolucao.guia_devolucao
+    guia_pagamento = guia_devolucao.guia_pagamento if guia_devolucao.present?
+    cliente        = guia_pagamento.cliente        if guia_pagamento.present? 
+    guia[:cliente] = cliente.nome if cliente.present?
+
+    guia[:tipo_debito] = descricao_de(guia_pagamento.debito_tipo) if guia_pagamento.present? 
+    
+    complemento(guia, devolucao)
+  end
+
+  def complemento(dados={}, devolucao)
+    dados[:valor_devolucao] = devolucao.valor_devolucao
+    dados[:data_devolucao] = devolucao.data_devolucao
+
+    situacao_anterior = devolucao.situacao_devolucao_anterior
+    dados[:situacao_anterior] = situacao_anterior.descricao_abreviada if situacao_anterior.present?
+    dados[:situacao_anterior_dica] = descricao_de situacao_anterior
+
+    situacao_atual = devolucao.situacao_devolucao_atual
+    dados[:situacao_atual]    = situacao_atual.descricao_abreviada if situacao_atual.present?
+    dados[:situacao_atual_dica] = descricao_de situacao_atual
+
+    dados
   end
 end
--- app/models/devolucao_situacao.rb
+++ app/models/devolucao_situacao.rb
@@ -4,7 +4,7 @@ class DevolucaoSituacao < ActiveRecord::Base
 
   alias_attribute "id",                  "dvst_id"
   alias_attribute "descricao",           "dvst_dsdevolucaosituacao"
-  alias_attribute "descricao_abreviado", "dvst_dsabreviado"
+  alias_attribute "descricao_abreviada", "dvst_dsabreviado"
   alias_attribute "ultima_alteracao",    "dvst_tmultimaalteracao"
   alias_attribute "indicador_uso",       "dvst_icuso"
   
